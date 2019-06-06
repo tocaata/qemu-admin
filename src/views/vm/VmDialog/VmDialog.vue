4dfc18a145d1e1aa2cc1bf479f6114db1fd2bbc7
tocaata <tuotaka@gmail.com>
2019-06-06 22:54:22 +0800

    Create Vm wizard: add uneditable vm arguments support. for example, -usb

--- src/views/vm/VmDialog/VmDialog.vue
+++ src/views/vm/VmDialog/VmDialog.vue
@@ -1,12 +1,12 @@
 <template>
   <el-dialog
-    modal
-    center
-    @close="reset"
-    :close-on-click-modal="false"
-    title="Create Machine"
-    width="40%"
-    :visible.sync="mutableVisible">
+      modal
+      center
+      @close="reset"
+      :close-on-click-modal="false"
+      title="Create Machine"
+      width="40%"
+      :visible.sync="mutableVisible">
     <div>
       <el-form :model="newVM" class="inline-form" label-width="auto">
         <div v-if="activeStep === 0">
@@ -19,12 +19,12 @@
           </el-form-item>
         </div>
 
-        <div v-for="(option, index) in allPrimaryOptions" :key="option.id">
+        <div v-for="(option, index) in configurableArgs" :key="option.id">
           <keep-alive>
             <kvm-wizard-control
-              v-if="activeStep === index + 1"
-              v-model="newVM.arguments[option.id]"
-              :config="option.config">
+                v-if="activeStep === index + 1"
+                v-model="newVM.arguments[option.id]"
+                :config="option.config">
             </kvm-wizard-control>
           </keep-alive>
         </div>
@@ -34,7 +34,7 @@
     <span slot="footer" class="dialog-footer">
       <el-button @click="mutableVisible = false">取 消</el-button>
       <el-button @click="activeStep -= 1" v-show="activeStep > 0">上一步</el-button>
-      <el-button type="primary" @click="activeStep += 1" v-if="activeStep <= allPrimaryOptions.length">下一步</el-button>
+      <el-button type="primary" @click="activeStep += 1" v-if="activeStep <= configurableArgs.length">下一步</el-button>
       <el-button type="primary" @click="createVm" v-else v-loading>创建</el-button>
     </span>
   </el-dialog>
@@ -70,19 +70,42 @@
           name: '',
           arguments: {}
         },
-        allPrimaryOptions: [],
+        configurableArgs: [],
+        unconfigArgs: []
       };
     },
     mounted () {
       getPrimaryOptions().then(res => {
-        this.allPrimaryOptions = res.data;
+        const allVmArgs = res.data.map(d => {
+          d.config = JSON.parse(d.config);
+          return d;
+        });
+
+        [this.configurableArgs, this.unconfigArgs] = allVmArgs.reduce((total, cur) => {
+          if (cur.config.params.length > 0) {
+            total[0].push(cur);
+          } else {
+            total[1].push(cur);
+          }
+          return total;
+        }, [[], []]);
+
+        Object.freeze(this.configurableArgs);
+        Object.freeze(this.unconfigArgs);
       });
     },
     methods: {
       createVm() {
         this.loading = true;
-        createVm(this.newVM).then(res => {
-          this.loading = false;
+        const tmpArgs = this.unconfigArgs.reduce((total, cur) => {
+          total[cur.id] = {};
+          return total;
+        }, {});
+
+        createVm({
+          name: this.newVM.name,
+          arguments: Object.assign({}, this.newVM.arguments, tmpArgs)
+        }).then(res => {
           this.loading = false;
           this.$message({
             type: 'success',
