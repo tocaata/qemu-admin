57508bf1bf45fe55758dbe4e4a67f2686e843dcd
tocaata <tuotaka@gmail.com>
2019-06-25 23:26:36 +0800

    start to develop create OS config dialog

---
+++ src/views/vm/components/NewOSConfDialog.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="display: inline-block">
+    <el-button icon="el-icon-plus" @click="dialogVisible = true" type="primary">
+      Create
+    </el-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NewOSConfDialog'
+  }
+</script>
+
+<style scoped>
+
+</style>
