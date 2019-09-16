802822ded8b2acd60c0c476607cdd2738f90a5cb
Toccata Yi <tuotaka@gmail.com>
2019-09-16 15:24:54 +0800

    use relative path to instead of the origin BASE_API for development envionment.

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  BASE_API: '"http://localhost:9527/api-dev"',
+  BASE_API: '"./api-dev"',
   SERVER: '"http://localhost:3000/"'
 }
