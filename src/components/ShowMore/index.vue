cefacf247a6b52df89b3dc69d2e74edb1051154a
tocaata <tuotaka@gmail.com>
2019-12-02 23:10:29 +0800

    hide surplus vm command arguments in vm detail

---
+++ src/components/ShowMore/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div @mouseenter="showLabel = true" @mouseleave="showLabel = false">
+    <slot :data="visibleData"></slot>
+    <div class="show-more" @click="isMore = !isMore" v-show="this.data && this.data.length > 5">
+      <i :class="{'el-icon-caret-bottom': !isMore, 'el-icon-caret-top': isMore, 'hovering': showLabel}"></i>
+      <span v-show="showLabel && !isMore">Show More</span>
+      <span v-show="showLabel && isMore">Hide More</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index',
+    props: {
+      data: {type: Array}
+    },
+    data() {
+      return {
+        isMore: false,
+        showLabel: false
+      };
+    },
+    computed: {
+      visibleData() {
+        if (this.isMore) {
+          return this.data;
+        } else {
+          return this.data && this.data.slice(0, 5);
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .show-more {
+    text-align: center;
+    color: #d3dce6;
+    width: 80%;
+    cursor: pointer;
+    line-height: 44px;
+
+    &:hover {
+      color: #409eff;
+    }
+
+    i {
+      transition: .8s;
+    }
+
+    span {
+      transition: .8s;
+    }
+  }
+</style>
