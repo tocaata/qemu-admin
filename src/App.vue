945ac2c8ff7b00362db8f351b7f9cc4aa5aed8ca
tocaata <tuotaka@gmail.com>
2020-01-04 17:35:53 +0800

    show message from websocket

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,16 @@
 
 <script>
 export default{
-  name: 'App'
+  name: 'App',
+  sockets: {
+    showMessage(data) {
+      console.log(data);
+
+      this.$message({
+        type: 'success',
+        message: data.message
+      });
+    }
+  }
 }
 </script>
